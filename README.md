0fb2592edb6f95ff034cf67c0cb6a8b8150879c8
Code Chip <willvix@outlook.com>
2025-02-02 22:57:50 -0300

    Base script addition.

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # job-stack
 Shellscript desktop linux
+
+# How Use
+1- Download the shellscript run command git clone git@github.com:code-chip/job-stack.git
+2- Access the fold with cd job-stack
+3- Addition permission for the file chmod 777 job-stack.sh
+4- Execute the script ./job-stack.sh
+
+## Package List
+# Develop
+-Git
+-Sublime-text
+-Docker
+-Docker composer
+-Insomnia
+-VSCode
+-DBeaver
+# Multimidia
+-Spotify
+-Flameshot
+-Kolourpaint
+-SimpleScreenrecorder
+# Monitoring
+-Htop
+-Bashtop
+-Psensor
+# Develop
---
+++ job-stack.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#packages repository develop
+sudo apt update && 
+apt install git sublime-text docker-compose docker-buildx -y &&
+sudo apt install -f
+sudo usermod -aG docker $USER &&
+
+#packages repository multimidia
+apt install spotify-client flameshot kolourpaint simplescreenrecorder -y &&
+
+#packages repository monitoring
+apt install remmina -y &&
+
+#packages monitoring
+apt install htop bashtop psensor -y &&
+
+#packages site develop
+curl -L "https://updates.insomnia.rest/downloads/ubuntu/latest?&app=com.insomnia.app&source=website" -o insomnia-lastest.deb &&
+sudo dpkg -i insomnia-lastest.deb &&
+sudo apt install -f &&
+rm insomnia-lastest.deb &&
+
+curl -L "https://code.visualstudio.com/docs/?dv=linux64_deb" -o vscode-lastest.deb &&
+sudo dpkg -i vscode-lastest.deb &&
+rm vscode-lastest.deb &&
+
+wget https://dbeaver.io/files/dbeaver-ce_latest_amd64.deb && sudo dpkg -i dbeaver-ce_latest_amd64.deb
